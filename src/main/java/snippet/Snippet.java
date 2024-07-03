13b862fc3178b5805b8c416b3a930553c106508f
Jagadeesh-maker <jagadeesh161293@gmail.com>
2024-07-03 21:45:03 +0530

    Project Structure

---
+++ src/main/java/snippet/Snippet.java
@@ -0,0 +1,49 @@
+package snippet;
+
+public class Snippet {
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git config --global user.email jagadeesh161293@gmail.com
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git config --global user.name Jagadeesh-maker
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git config --global user.email
+	jagadeesh161293@gmail.com
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git config --global user.name
+	Jagadeesh-maker
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git init
+	Reinitialized existing Git repository in C:/Users/admin/eclipse-workspace/ProjectClass08.00AM_July24/.git/
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git status
+	On branch master
+	nothing to commit, working tree clean
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git add .
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git status
+	On branch master
+	nothing to commit, working tree clean
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$ git commit -m "Project Structure"
+	On branch master
+	nothing to commit, working tree clean
+	
+	admin@DESKTOP-QNNONG5 MINGW64 ~/eclipse-workspace/ProjectClass08.00AM_July24 (master)
+	$
+	
+	
+	
+	
+	
+}
+
